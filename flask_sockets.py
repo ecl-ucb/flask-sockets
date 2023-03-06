6cc88f9f77156f5afc24d3f3b492db94692ed1a8
wdt <ecl-ucb@berkeley.edu>
2023-03-06 15:06:31 -0800

    removing problematic line

--- flask_sockets.py
+++ flask_sockets.py
@@ -106,8 +106,8 @@ if ('Worker' in locals() and 'PyWSGIHandler' in locals() and
 
     class GunicornWebSocketHandler(PyWSGIHandler, WebSocketHandler):
         def log_request(self):
-            if '101' not in self.status:
-                super(GunicornWebSocketHandler, self).log_request()
+            # if '101' not in self.status:
+            super(GunicornWebSocketHandler, self).log_request()
 
     Worker.wsgi_handler = GunicornWebSocketHandler
     worker = Worker
